9fdac2851149f91dacb091027786933fb0e3f873
goo <goo>
2022-12-11 11:32:08 +0200

    set a reducer

--- src/context/UsersContext.js
+++ src/context/UsersContext.js
@@ -1,20 +1,38 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
 
 export const UsersContext = createContext();
 
+export const usersReducer = (state, action) => {
 
+    switch (action.type) {
+        case 'ADD_USER':
+            return
+            break;
+        case 'EDIT_USER':
+            return
+            break;
+        default:
+            return state;
+    }
+}
 
 
 
 export function UsersProvider({ children }) {
 
-    const users = ['bb', 'sara'];
-    const kunifunc = () => {
-        console.log('hi kuni');
-    }
+    const [state, dispatch] = useReducer(usersReducer, {
+        users: [
+            // users: ['bb', 'sara'],
+            { 'RUindex': 'bb the king', 'Data Rate': 7, 'DCM': false, 'WIFI Code': 0, 'nSS': 1, 'sSS': 1 },
+            { 'RUindex': 'sara the queen','Data Rate': 7, 'DCM': false, 'WIFI Code': 0, 'nSS': 1, 'sSS': 1 }
+        ],
+        kuni:'ahmed'
+    });
+
+
 
     return (
-        <UsersContext.Provider value={{ users, kunifunc }}>
+        <UsersContext.Provider value={{ ...state }}>
             {children}
         </UsersContext.Provider>
     );
