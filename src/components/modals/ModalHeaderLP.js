7b7a88c2340fcc86c6b67ac448b1545ddb092d22
goo <goo>
2022-12-18 15:04:27 +0200

    added icon logo to 'done' btn and some transition

--- src/components/modals/ModalHeaderLP.js
+++ src/components/modals/ModalHeaderLP.js
@@ -11,26 +11,32 @@ export default function ModalHeaderLP() {
         if (users.length) {
             setShowBtn(true);
         }
-        else{
+        else {
             setShowBtn(false);
         }
     }, [users]);
 
     // printing json array of users
-    const handleClick = () => {        
+    const handleClick = () => {
         console.log('users from done:', users);
         const usersJson = JSON.stringify(users);
         console.log('users as JSON:', usersJson);
-}
+    }
     return (
         <div className="modal-header-lp">
             <h2>
                 Click on RU to add user
-                {showBtn && <span id="close-modal" className="clickable" onClick={handleClick}
+                {/* <img src="done_all_FILL0_wght400_GRAD0_opsz48.svg" style={{color:'red'}}/>*/}
+
+
+                {/* {showBtn && <span id="close-modal" className="clickable" onClick={handleClick}
                     style={{ paddingLeft: '450px', width: '40px' }}>
                     Done
-                </span>}
+                </span>} */}
             </h2>
+            {<span id="close-modal" className={`material-symbols-outlined ${!showBtn? "hidden":"show clickable"}`}
+            onClick={handleClick}
+            style={{ fontSize: '72px' }}>done_all</span>}
         </div>
     );
 }
